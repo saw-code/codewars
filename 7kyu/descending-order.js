371fe36c2ceab81a319b6e9e0051b455edabeffd
Dmitry Pilavskiy <saw.coding@gmail.com>
2025-12-18 06:01:21 +0300

    Add solution for "Descending Order"

---
+++ 7kyu/descending-order.js
@@ -0,0 +1,41 @@
+//❓🤔DESCRIPTION:
+// Your task is to make a function that can take any non-negative integer as an
+// argument and return it with its digits in descending order. Essentially,
+// rearrange the digits to create the highest possible number.
+//
+// Examples:
+// Input: 42145 Output: 54421
+//
+// Input: 145263 Output: 654321
+//
+// Input: 123456789 Output: 987654321
+
+// function descendingOrder(n){
+//   //...
+// }
+
+// 🐥TESTS:
+// const { assert } = require("chai")
+//
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(descendingOrder(0), 0)
+//     assert.strictEqual(descendingOrder(1), 1)
+//     assert.strictEqual(descendingOrder(111), 111)
+//     assert.strictEqual(descendingOrder(15), 51)
+//     assert.strictEqual(descendingOrder(1021), 2110)
+//     assert.strictEqual(descendingOrder(123456789), 987654321)
+//   })
+// })
+
+
+// ✅ SOLUTION:
+
+function descendingOrder(n){
+  const numbersArray = Array.from(String(n), Number)
+  const number = numbersArray.sort((a, b) => b - a)
+  return Number(number.join(''))
+}
+
+console.log(descendingOrder(123456789))
+console.log(descendingOrder(1021))
